1bdc6295d92fb4b6c6513388de50dba4f5b4b79f
Bob.C <bob.cocksedge@gmail.com>
2021-03-07 16:04:36 +0000

    Complete bindings and scopes, nested scope and lexical binding, function values - break off and go to chapter 5 see note on index page Create index5, jscript5 style5 for exercises

--- jscript.js
+++ jscript.js
@@ -1,21 +1,64 @@
 // defining a function expression
-const square= function(x){
-    return (x * x);
-};
-console.log(square(12));
+// const square= function(x){
+//     return (x * x);
+// };
+// console.log(square(12));
 
-// parameters
-const makenoise= function(){
-    console.log("Pling!");
-};
-makenoise();
+// no parameters
+//const makenoise= function(){
+    //console.log("Pling!");
+//     return("Pling");       
+// };
+//makenoise();
+//console.log(makenoise());
 
-const power= function(base, exponent){
-    let result= 1;
-    for (let count= 0; count < exponent; count++) {
-        result *= base;
-    }
-    return; //result;
-}
-    console.log(power(2, 10));
+//two parameters
+// const power= function(base, exponent){
+//     let result= 1;
+//     for (let count= 0; count < exponent; count++) {
+//         result *= base;
+//     }
+//     return(result);
+// }
+//     console.log(power(2, 10));
+
+//bindings and scopes
+// let x= 10;
+// if (true){
+//     let y= 20;
+//     var z= 30;
+//     console.log(x + y + z);
+// }
+//     console.log(x + y + z); //-> y is not defined - outside scope of function!
+
+    //let n= 10;
+//    const halve= function(n) {
+        //let n= 10;
+//         return n / 2;
+// };
+//      let n= 10;                 //cannot be seen by function
+    //console.log(halve(100));
+    // console.log(halve(10));
+    // console.log(n);             //cannot be seen by function only sees let n= 10;
+
+//nested scope and lexical binding
+// const hummus= function(batches) {                   //factor changed to batches - more logical to me!
+//     const ingredient= function(amount, unit, name){
+//         let ingredientAmount= amount * batches;
+//         if(ingredientAmount > 1) {
+//             unit += 's';
+//         }
+//         console.log(`${ingredientAmount} ${unit} ${name}`);
+//     };
+//         ingredient( 1, "can", "chickpeas");
+//         ingredient( 0.25, "cup", "tahini");
+//         ingredient( 0.25, "cup", "lemon juice");
+//         ingredient( 1, "clove", "garlic");
+//         ingredient( 2, "tablespoon", "olive oil");
+//         ingredient( 0.5, "teaspoon", "cumin");
+// };
+//         hummus(5);
+
+
+
 
